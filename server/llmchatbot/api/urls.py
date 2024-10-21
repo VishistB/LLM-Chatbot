f8d47fc08b60d7adc2d21183815a53110654c340
Vishist Bhagabati <vishist.bhagabati123@gmail.com>
2024-10-21 15:11:07 +0530

    set up layout for chatbot integration

--- server/llmchatbot/api/urls.py
+++ server/llmchatbot/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.endpointview.as_view(),name='endpoints')
+    path('',views.endpointview.as_view(),name='endpoints'),\
+    path('prompts/',views.PromptResponseView.as_view(),name='prompts')
 ]
--- server/llmchatbot/api/views.py
+++ server/llmchatbot/api/views.py
@@ -3,9 +3,33 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
+# from langchain_huggingface import HuggingFaceEndpoint
+import environ
+
+# env = environ.Env()
+# HUGGINGFACE_API_KEY = env('HUGGINGFACE_API_KEY')
+# repo_id="mistralai/Mistral-7B-Instruct-v0.3"
+# llm=HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7, token=HUGGINGFACE_API_KEY)
+
+# environ.Env.read_env()
 
 # Create your views here.
 
 class endpointview(APIView):
     def get(self,request):
         return Response({"Rest-Api running : successful"},status=status.HTTP_200_OK)
+    
+
+class PromptResponseView(APIView):
+    def post(self, request):
+        try:
+            prompt = request.data.get('prompt')
+            if not prompt:
+                return Response({"error": "No prompt provided"}, status=status.HTTP_400_BAD_REQUEST)
+            
+            # generated_text = llm.invoke(prompt)
+
+            return Response({"response": "test"}, status=status.HTTP_200_OK)
+            
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
