95afec8ecca755b558367f2ac681a7773762351e
Vishist Bhagabati <vishist.bhagabati123@gmail.com>
2024-10-30 23:42:16 +0530

    integrated Mistral AI model with server

--- server/llmchatbot/api/views.py
+++ server/llmchatbot/api/views.py
@@ -3,14 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import HttpResponse
-# from langchain_huggingface import HuggingFaceEndpoint
+from langchain_huggingface import HuggingFaceEndpoint
 import environ
+from time import sleep
 
-# env = environ.Env()
-# HUGGINGFACE_API_KEY = env('HUGGINGFACE_API_KEY')
-# repo_id="mistralai/Mistral-7B-Instruct-v0.3"
-# llm=HuggingFaceEndpoint(repo_id=repo_id, max_length=128, temperature=0.7, token=HUGGINGFACE_API_KEY)
-
+env = environ.Env()
 # environ.Env.read_env()
 
 # Create your views here.
@@ -27,9 +24,11 @@ class PromptResponseView(APIView):
             if not prompt:
                 return Response({"error": "No prompt provided"}, status=status.HTTP_400_BAD_REQUEST)
             
-            # generated_text = llm.invoke(prompt)
-
-            return Response({"response": "test"}, status=status.HTTP_200_OK)
+            HUGGINGFACE_API_KEY = env('HUGGINGFACE_API_KEY')
+            repo_id="mistralai/Mistral-7B-Instruct-v0.3"
+            llm=HuggingFaceEndpoint(repo_id=repo_id,max_length=128,temperature=0.7,api=HUGGINGFACE_API_KEY)
+            ans=llm.invoke(prompt)
+            return Response({"response": ans}, status=status.HTTP_200_OK)
             
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
