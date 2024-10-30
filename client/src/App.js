5834f25c588a9d30e3b33aeabf666ec98edfddc6
Vishist Bhagabati <vishist.bhagabati123@gmail.com>
2024-10-31 02:46:48 +0530

    non responsive frontend layout made

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Drawer from '@mui/material/Drawer';
+import { Stack, Divider, Box } from "@mui/material";
+import Logo from './Assets/Mimir_Logo.png';
+import ResponsiveAppBar from  "./Components/ResponsiveAppBar";
+import ChatPage from "./Pages/ChatPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: "flex", background: "#1A1D20", height: "100vh" }}>
+      {/* Drawer */}
+      <Drawer
+        sx={{
+          width: 250,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            backgroundColor: "#23262A",
+            width: 250,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <img src={Logo} width="200px" style={{ margin: "0 auto" }} />
+        <Divider />
+      </Drawer>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          // marginLeft: { xs: 0, sm: "250px" },
+        }}
+      >
+        {/* AppBar */}
+        <ResponsiveAppBar />
+
+        {/* ChatPage to fill remaining space */}
+        <Box sx={{ flex: 1, overflow: "auto" }}>
+          <ChatPage />
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
---
+++ client/src/Pages/ChatPage.js
@@ -0,0 +1,10 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+export default function ChatPage() {
+  return (
+    <Stack height="100%" sx={{color:"white"}}>
+        
+    </Stack>
+  )
+}
